ceed51ed37f4ee823257690d5682015de2c8072e
Saleh BA-ZIGHIFAN <saleh738297@gmail.com>
2026-02-26 18:08:05 +0100

    Refactor database config to use environment variables
    Updated database configuration to use environment variables for sensitive information.

--- app/config/config.php
+++ app/config/config.php
@@ -1,15 +1,10 @@
-
-
 <?php
 
-// Fichier de configuration de la connexion à la base de données (PDO)
-// Contient les paramètres nécessaires : host, port, nom de la BDD et identifiants
-
 return [
-  'host' => 'localhost',       // serveur MySQL
-  'port' => 3307,             // port utilisé par MySQL
-  'dbname' => 'filrouge',    // nom de la base de données
-  'user' => 'root',          // utilisateur MySQL
-  'pass' => 'S@leh',         // mot de passe
-  'charset' => 'utf8mb4'      // encodage pour supporter tous les caractères
+  'host' => getenv('MYSQLHOST'),
+  'port' => getenv('MYSQLPORT'),
+  'dbname' => 'filrouge',
+  'user' => getenv('MYSQLUSER'),
+  'pass' => getenv('MYSQLPASSWORD'),
+  'charset' => 'utf8mb4'
 ];
