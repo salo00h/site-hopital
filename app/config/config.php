b8af4040eb787c9c9ad4724364219773bd42e38e
Saleh BA-ZIGHIFAN <saleh738297@gmail.com>
2026-02-26 19:00:40 +0100

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -1,12 +1,8 @@
-<?php
-
 return [
-
-  'host' => getenv('DB_HOST'),
-  'port' => getenv('DB_PORT'),
-  'dbname' => getenv('DB_NAME'),
-  'user' => getenv('DB_USER'),
-  'pass' => getenv('DB_PASS'),
+  'host' => getenv('DB_HOST') ?: getenv('MYSQLHOST'),
+  'port' => getenv('DB_PORT') ?: getenv('MYSQLPORT') ?: 3306,
+  'dbname' => getenv('DB_NAME') ?: getenv('MYSQLDATABASE') ?: 'filrouge',
+  'user' => getenv('DB_USER') ?: getenv('MYSQLUSER'),
+  'pass' => getenv('DB_PASS') ?: getenv('MYSQLPASSWORD'),
   'charset' => 'utf8mb4'
-
 ];
